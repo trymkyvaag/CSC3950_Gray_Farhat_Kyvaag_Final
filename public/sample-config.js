093a8531c122d18bfb81041089bc8f2054f51d1d
Doodle173 <dkgray01@gmail.com>
2023-12-07 19:22:47 -0500

    added login demo from firbase github

---
+++ public/sample-config.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var config = {
+  apiKey: "AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY",
+  authDomain: "webdevfinal-1b6d7.firebaseapp.com",
+  projectId: "webdevfinal-1b6d7",
+  storageBucket: "webdevfinal-1b6d7.appspot.com",
+  messagingSenderId: "831425261552",
+  appId: "1:831425261552:web:69f0d0e1e7a5193ef57735",
+  measurementId: "G-CJPK72JB1N"
+};
+firebase.initializeApp(config);
+
+
+// Google OAuth Client ID, needed to support One-tap sign-up.
+// Set to null if One-tap sign-up is not supported.
+var CLIENT_ID =
+    'YOUR_OAUTH_CLIENT_ID';
