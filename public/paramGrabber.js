8de51f0f21a3d89edd459f0338a5d8c915b7b8ef
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-12-11 10:51:52 -0500

    given auth, get uid

--- public/paramGrabber.js
+++ public/paramGrabber.js
@@ -1,40 +1,134 @@
-var user_key = "AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY"; //TODO: Need to change this and set dynamically
+// var user_key = "AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY"; //TODO: Need to change this and set dynamically
 
-function getUserIDFromBookshelf() {
-    // Make an authenticated request to get a Bookshelf resource
-    return fetchBookshelfResource()
-        .then(res => res.json())
-        .then(data => {
+// function getUserIDFromBookshelf() {
+//     // Make an authenticated request to get a Bookshelf resource
+//     return fetchBookshelfResource()
+//         .then(res => res.json())
+//         .then(data => {
 
-            // Check if the selfLink property exists in the Bookshelf resource
-            if (data && data.selfLink) {
-                var userId = extractUserIDFromSelfLink(data.selfLink);
-                return userId;
-            } else {
-                console.log(data.data)
-                throw new Error("Unable to retrieve user ID from Bookshelf resource.");
-            }
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
-}
+//             // Check if the selfLink property exists in the Bookshelf resource
+//             if (data && data.selfLink) {
+//                 var userId = extractUserIDFromSelfLink(data.selfLink);
+//                 return userId;
+//             } else {
+//                 console.log(data.data)
+//                 throw new Error("Unable to retrieve user ID from Bookshelf resource.");
+//             }
+//         })
+//         .catch(error => {
+//             console.error("Error:", error);
+//         });
+// }
+
+// async function fetchBookshelfResourceFromURL(url) {
+//     try {
+//         // Extract UID from the provided URL
+//         const userId = extractUserIDFromSelfLink(url);
+
+//         // Append UID to the base URL
+//         const apiUrl = `https://books.google.com/books?uid=${userId}`;
+//         console.log('API URL:', apiUrl);
+
+//         // Fetch the resource
+//         const response = await fetch(apiUrl);
+
+//         if (response.ok) {
+//             console.log(response);
+//             const data = await response.json();
+//             return { success: true, data };
+//         } else {
+//             console.error('Failed to fetch resource');
+//             return { success: false, error: 'Failed to fetch resource' };
+//         }
+//     } catch (error) {
+//         console.error('Error:', error);
+//         return { success: false, error: 'Server error' };
+//     }
+// }
 
 function extractUserIDFromSelfLink(selfLink) {
-    var pathSegments = new URL(selfLink).pathname.split('/');
-    var userId = pathSegments[pathSegments.length - 1];
-    return userId;
+    try {
+        const url = new URL(selfLink);
+        const searchParams = new URLSearchParams(url.search);
+        const userId = searchParams.get('uid');
+        return userId;
+    } catch (error) {
+        console.error('Error extracting userID:', error);
+        return null;
+    }
 }
 
-function fetchBookshelfResource() {
+async function fetchBookshelfResourceFromURL(url) {
+    try {
+        // Extract UID from the provided URL
+        const userId = extractUserIDFromSelfLink(url);
 
-    var url = 'https://www.googleapis.com/books/v1/mylibrary/bookshelves/0';
-    url += `&key=${user_key}`;
-    console.log('URL:' + url)
-    return fetch(url);
+        if (!userId) {
+            console.error('UID not found in the URL');
+            return { success: false, error: 'UID not found in the URL' };
+        }
+
+        // Append UID to the base URL
+        const apiUrl = `https://books.google.com/books?uid=${userId}`;
+        console.log('API URL:', apiUrl);
+        console.log("userID: " + userId);
+
+        // Fetch the resource
+        const response = await fetch(apiUrl);
+
+        console.log('Response URL:', response.url);
+
+        if (response.ok) {
+            // Handle HTML content separately
+            const htmlContent = await response.text();
+
+            // console.log('HTML Content:', htmlContent);
+            return { success: true, htmlContent };
+        } else {
+            console.error('Failed to fetch resource');
+            return { success: false, error: 'Failed to fetch resource' };
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return { success: false, error: 'Server error' };
+    }
 }
 
+// function createGoogleBooksLink(clientId, clientSecret) {
+//     const encodedClientId = encodeURIComponent(clientId);
+//     const encodedClientSecret = encodeURIComponent(clientSecret);
 
-getUserIDFromBookshelf().then(userId => {
-    console.log("User ID:", userId);
-});
+//     const link = `https://books.google.com/books?uid=${encodedClientId}&source=gbs_lp_bookshelf_list`;
+
+//     return link;
+// }
+
+
+
+const url = 'https://example.com/books?uid=102648910697821551523';
+fetchBookshelfResourceFromURL(url)
+    .then(result => {
+        if (result.success) {
+            console.log('Success')//'HTML Content:', result.htmlContent);
+        } else {
+            console.error('Error:', result.error);
+        }
+    });
+
+
+
+// // Example usage:
+// const googleBooksAPIURL = 'https://www.googleapis.com/books/&key=AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY';
+// const key = '831425261552-etagdiu9au5k6qpra3cm6f3mth6379tn.apps.googleusercontent.com';
+
+// const extractedUID = extractUIDFromGoogleBooksAPI(googleBooksAPIURL, key, title);
+
+// console.log('Extracted UID:', extractedUID);
+
+
+// // Example usage
+// const clientId = '831425261552-etagdiu9au5k6qpra3cm6f3mth6379tn.apps.googleusercontent.com';
+// const clientSecret = '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';
+
+// const googleBooksLink = createGoogleBooksLink(clientId, clientSecret);
+// console.log(googleBooksLink);
