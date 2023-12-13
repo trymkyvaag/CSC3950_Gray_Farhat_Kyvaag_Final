6cc6724aa009e116da91d8705dff3a1ed84bee90
Trym Kyvåg <trymkyvag@Tryms-MacBook-Pro.local>
2023-12-13 16:41:27 -0500

    add or check to db works

--- public/javascript/addCheckUser.js
+++ public/javascript/addCheckUser.js
@@ -15,11 +15,51 @@ const database = db.getDatabase();
 
 const rootRef = db.ref(database);
 
-const newData = {
-    email: "john@example.com",
-    uid: 123
-  };
+// const newData = {
+//     email: "john@example.com",
+//     uid: 123
+// };
 
-const newEntryRef = db.push(rootRef, newData);
-const newEntryKey = newEntryRef.key;
-console.log(`New entry added with key: ${newEntryKey}`);
+// const newEntryRef = db.push(rootRef, newData);
+// const newEntryKey = newEntryRef.key;
+// console.log(`New entry added with key: ${newEntryKey}`);
+
+
+async function checkAndAddUser(user) {
+    try {
+        // Check if user with the same UID exists
+        console.log(user.uid)
+        // const userQuery = db.query(rootRef, db.equalTo('uid', user.uid));
+
+        // const userSnapshot = await db.get(userQuery);
+        const snapshot = await db.get(rootRef);
+        const users = snapshot.val();
+
+        // Check if user with the same UID already exists
+        const userExists = Object.values(users).some(existingUser => existingUser.uid === user.uid);
+
+
+        if (userExists) {
+            console.log('User already exists in the database');
+        } else {
+            await db.push(rootRef, user);
+            console.log('New user added to the database');
+        }
+    } catch (error) {
+        console.error('Error checking and adding user:', error.message);
+    }
+}
+
+// Example usage
+// const newUser = {
+//     email: "tom@example.com",
+//     uid: "1223"
+// };
+
+// checkAndAddUser(newUser);
+
+
+async function get(query) {
+    const snapshot = await query;
+    return snapshot.val();
+}
