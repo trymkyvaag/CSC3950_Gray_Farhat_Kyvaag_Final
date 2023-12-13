c67b64bc56f7e4b92eced797251541dfaa77735f
Doodle173 <dkgray01@gmail.com>
2023-12-13 11:34:24 -0500

    send_authorized promisified

--- public/javascript/booktesting.js
+++ public/javascript/booktesting.js
@@ -14,27 +14,28 @@ function getBookshelf(shelf_index) {
 
 }
 
-function getAllBookshelves(){
+async function getAllBookshelves() {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves`;
-    send_authorized(url, "GET");
+    var result = await send_authorized(url, "GET");
+    return result;
 }
 
-function getBooksFromShelf(shelf_idx){
+function getBooksFromShelf(shelf_idx) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_idx}/volumes`;
     send_authorized(url, "GET");
 }
 
-function clearBooksFromShelf(shelf_idx){
+function clearBooksFromShelf(shelf_idx) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/9/clearVolumes?key=AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY`;
     send_authorized(url, "POST");
 }
 
-function addBookToShelf(book_id, shelf_index){
+function addBookToShelf(book_id, shelf_index) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}/addVolume?volumeId=${book_id}&country=US`
     send_authorized(url, "POST");
 }
 
-function removeBookFromShelf(book_id, shelf_index){
+function removeBookFromShelf(book_id, shelf_index) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}/removeVolume?volumeId=${book_id}&country=US`
     send_authorized(url, "POST");
 }
@@ -43,22 +44,29 @@ function removeBookFromShelf(book_id, shelf_index){
 // addBookToShelf("NRWlitmahXkC");
 
 
-function send_authorized(url, method){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    };
-    xhttp.open(method, url, true);
-
+function send_authorized(url, method) {
+    var res;
     var token = "Bearer " + sessionStorage.getItem("oauth_token");
-
-    xhttp.setRequestHeader("Authorization", token);
-    xhttp.send();
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': token,
+        },
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var userid = JSON.stringify(data);
+        return userid;
+    });
 }
 
-function send_unauthorized(url){
+var x = getAllBookshelves().then(result => {
+    console.log(result);
+}).catch(error => {
+
+})
+
+function send_unauthorized(url) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
