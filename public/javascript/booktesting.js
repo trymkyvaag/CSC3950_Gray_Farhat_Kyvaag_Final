09ae6b956d27da180d1942ad15dabfc58e662519
Doodle173 <dkgray01@gmail.com>
2023-12-13 11:58:39 -0500

    Booktesting.js documentation

--- public/javascript/booktesting.js
+++ public/javascript/booktesting.js
@@ -2,12 +2,34 @@ var url = ``;
 var key = `AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY`;
 var client_id = '';
 
-async function testBookApi(title, author) {
+/**
+ * 
+ * Saving promise results:
+ * 
+ * https://stackoverflow.com/questions/64164994/is-there-a-way-to-save-promise-result-as-a-variable
+ */
+
+
+/**
+ * 
+ * Searches for a book based on the given parameters
+ * 
+ * @param {*} title The title of the book
+ * @param {*} author The author of the book
+ * @returns Search results in JSON format
+ */
+async function searchForBook(title, author) {
     url = `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=${key}`;
     var result = await send_unauthorized(url, "GET");
     return result;
 }
 
+/**
+ * Returns a bookshelf based on the index parameter.
+ * 
+ * @param {*} shelf_index The index of the shelf we're looking for 
+ * @returns Bookshelf in JSON format
+ */
 async function getBookshelf(shelf_index) {
 
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}`;
@@ -15,36 +37,73 @@ async function getBookshelf(shelf_index) {
     return result;
 }
 
+/**
+ * @returns Returns a list of all of a user's bookshelves
+ */
 async function getAllBookshelves() {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves`;
     var result = await send_authorized(url, "GET");
     return result;
 }
 
+/**
+ * Returns a list of all the books within a specified bookshelf
+ * @param {*} shelf_idx The index of the shelf we're looking for
+ * @returns A list of books in a given bookshelf, in JSON format
+ */
 async function getBooksFromShelf(shelf_idx) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_idx}/volumes`;
     var result = await send_authorized(url, "GET");
     return result;
 }
 
+/**
+ * Clears all books from a given bookshelf.
+ * 
+ * @param {*} shelf_idx The index of the shelf we're looking for
+ * @returns Should return an empty array.
+ */
 async function clearBooksFromShelf(shelf_idx) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_idx}/clearVolumes?key=${key}`;
     var result = await send_authorized(url, "POST");
     return result;
 }
 
+/**
+ * 
+ * Adds a given book to a given bookshelf.
+ * 
+ * @param {*} book_id The ID of the book we want to add
+ * @param {*} shelf_index The index of the shelf we're looking for
+ * @returns 
+ */
 async function addBookToShelf(book_id, shelf_index) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}/addVolume?volumeId=${book_id}&country=US`
     var result = await send_authorized(url, "POST");
     return result;
 }
 
+/**
+ * Removes a given book from a given bookshelf.
+ * 
+ * @param {*} book_id The ID of the book we want to add
+ * @param {*} shelf_index The index of the shelf we're looking for
+ * @returns 
+ */
 async function removeBookFromShelf(book_id, shelf_index) {
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}/removeVolume?volumeId=${book_id}&country=US`
     var result = await send_authorized(url, "POST");
     return result;
 }
 
+/**
+ * 
+ * Sends an authorized HTTP request to the Google Books API for private Google Books data
+ * 
+ * @param {*} url The query URL
+ * @param {*} method The HTTP method
+ * @returns Returns the result of the HTTP request
+ */
 function send_authorized(url, method) {
     var token = "Bearer " + sessionStorage.getItem("oauth_token");
     var httpmethod = method;
@@ -61,6 +120,14 @@ function send_authorized(url, method) {
     });
 }
 
+/**
+ * 
+ * Sends a generic, unauthorized HTTP request to the Google Books API for public Google Books data.
+ * 
+ * @param {*} url The query URL
+ * @param {*} method The HTTP method
+ * @returns Returns the result of the HTTP request
+ */
 function send_unauthorized(url, method) {
     var httpmethod = method;
     return fetch(url, {
