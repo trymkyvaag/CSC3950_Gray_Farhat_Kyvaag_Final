113958b80b7b3333d0988c0508596833612ce409
Doodle173 <dkgray01@gmail.com>
2023-12-12 19:42:27 -0500

    added func to get all user bookshelves

--- public/javascript/booktesting.js
+++ public/javascript/booktesting.js
@@ -1,15 +1,22 @@
-var client_id = "AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY";
-var daniel_uid = "113703336771319867948";
+var url = ``;
+
+
+
 function testBookApi(title, author) {
-    var url = `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=${client_id}`;
+    url = `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=${client_id}`;
     send_unauthorized(url);
 }
 
 function getBookshelf(shelf_index) {
 
-    var url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}`;
+    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}`;
     sendAuthorized(url);
 
+}
+
+function getAllBookshelves(){
+    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves`;
+    send_authorized(url);
 }
 
 function send_authorized(url){
@@ -22,7 +29,6 @@ function send_authorized(url){
     xhttp.open("GET", url, true);
 
     var token = "Bearer " + sessionStorage.getItem("oauth_token");
-    console.log(token);
 
     xhttp.setRequestHeader("Authorization", token);
     xhttp.send();
@@ -38,14 +44,9 @@ function send_unauthorized(url){
     xhttp.open("GET", url, true);
 
     var token = "Bearer " + sessionStorage.getItem("oauth_token");
-    console.log(token);
 
     xhttp.setRequestHeader("Authorization", token);
     xhttp.send();
 }
-function testBookshelfFunc() {
-    console.log("DBG> Public bookshelf (0): ");
-    getBookshelf(0);
-}
 
-testBookApi();
+getAllBookshelves();
