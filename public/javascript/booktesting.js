685834401c0b9715d2f36454db7abf2bfa81dd4b
Doodle173 <dkgray01@gmail.com>
2023-12-12 19:40:05 -0500

    made api query code reuseable

--- public/javascript/booktesting.js
+++ public/javascript/booktesting.js
@@ -2,23 +2,19 @@ var client_id = "AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY";
 var daniel_uid = "113703336771319867948";
 function testBookApi(title, author) {
     var url = `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=${client_id}`;
-
-
-    let test = fetch(url);
-    test.then(res =>
-        res.json()).then(d => {
-            console.log(d)
-        })
+    send_unauthorized(url);
 }
-
-// getBookshelves(daniel_uid);
 
 function getBookshelf(shelf_index) {
 
     var url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}`;
+    sendAuthorized(url);
 
+}
+
+function send_authorized(url){
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
         }
@@ -27,13 +23,29 @@ function getBookshelf(shelf_index) {
 
     var token = "Bearer " + sessionStorage.getItem("oauth_token");
     console.log(token);
-        
+
     xhttp.setRequestHeader("Authorization", token);
     xhttp.send();
 }
 
-console.log("DBG> Public bookshelf (0): ");
-getBookshelf(0);
+function send_unauthorized(url){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", url, true);
 
-console.log("DBG> Private bookshelf (1): ");
-getBookshelf(1);
+    var token = "Bearer " + sessionStorage.getItem("oauth_token");
+    console.log(token);
+
+    xhttp.setRequestHeader("Authorization", token);
+    xhttp.send();
+}
+function testBookshelfFunc() {
+    console.log("DBG> Public bookshelf (0): ");
+    getBookshelf(0);
+}
+
+testBookApi();
