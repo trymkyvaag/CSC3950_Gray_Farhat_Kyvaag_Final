84a80e6957290ea1b7a00018ae5b2fc0dc1b7dee
Doodle173 <dkgray01@gmail.com>
2023-12-12 20:46:41 -0500

    add book to shelf func works now

--- public/javascript/booktesting.js
+++ public/javascript/booktesting.js
@@ -1,5 +1,5 @@
 var url = ``;
-
+var key = `AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY`;
 
 
 function testBookApi(title, author) {
@@ -10,23 +10,40 @@ function testBookApi(title, author) {
 function getBookshelf(shelf_index) {
 
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_index}`;
-    sendAuthorized(url);
+    send_authorized(url, "GET");
 
 }
 
 function getAllBookshelves(){
     url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves`;
-    send_authorized(url);
+    send_authorized(url, "GET");
 }
 
-function send_authorized(url){
+function getBooksFromShelf(shelf_idx){
+    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_idx}/volumes`;
+    send_authorized(url, "GET");
+}
+
+function clearBooksFromShelf(shelf_idx){
+    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/9/clearVolumes?key=AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY`;
+    send_authorized(url, "POST");
+}
+
+function addBookToShelf(book_id){
+    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/0/addVolume?volumeId=${book_id}&country=US`
+    send_authorized(url, "POST");
+}
+addBookToShelf("NRWlitmahXkC");
+
+
+function send_authorized(url, method){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
         }
     };
-    xhttp.open("GET", url, true);
+    xhttp.open(method, url, true);
 
     var token = "Bearer " + sessionStorage.getItem("oauth_token");
 
@@ -48,11 +65,3 @@ function send_unauthorized(url){
     xhttp.setRequestHeader("Authorization", token);
     xhttp.send();
 }
-
-function getBooksFromShelf(shelf_idx){
-    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/${shelf_idx}/volumes`;
-    send_authorized(url);
-}
-
-
-// getBooksFromShelf();
