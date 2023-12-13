d45eb8cfc68385052aab17204676484ca2d3a99f
Doodle173 <dkgray01@gmail.com>
2023-12-13 11:45:42 -0500

    all http get methods promisified

--- public/javascript/booktesting.js
+++ public/javascript/booktesting.js
@@ -1,10 +1,11 @@
 var url = ``;
 var key = `AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY`;
+var client_id = '';
 
-
-function testBookApi(title, author) {
-    url = `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=${client_id}`;
-    send_unauthorized(url);
+async function testBookApi(title, author) {
+    url = `https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=${key}`;
+    var result = await send_unauthorized(url, "GET");
+    return result;
 }
 
 async function getBookshelf(shelf_index) {
@@ -27,7 +28,7 @@ async function getBooksFromShelf(shelf_idx) {
 }
 
 function clearBooksFromShelf(shelf_idx) {
-    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/9/clearVolumes?key=AIzaSyDC9zpVAnoBNi0T__-qUv6PbdA_sgrnbGY`;
+    url = `https://www.googleapis.com/books/v1/mylibrary/bookshelves/9/clearVolumes?key=${key}`;
     send_authorized(url, "POST");
 }
 
@@ -57,26 +58,34 @@ function send_authorized(url, method) {
     });
 }
 
-function send_unauthorized(url) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    };
-    xhttp.open("GET", url, true);
-
-    var token = "Bearer " + sessionStorage.getItem("oauth_token");
-
-    xhttp.setRequestHeader("Authorization", token);
-    xhttp.send();
+function send_unauthorized(url, method) {
+    var res;
+    return fetch(url, {
+        method: 'GET',
+        headers: {},
+    }).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var userid = JSON.stringify(data);
+        return userid;
+    });
 }
+
 
 /**
  * An example of getting all of a user's bookshelves and then printing it to console
  */
-var x = getAllBookshelves.then(result => {
-    console.log(result);
-}).catch(error => {
+// var x = getAllBookshelves.then(result => {
+//     console.log(result); //prints out the json response
+// }).catch(error => {
 
-})
+// })
+
+/**
+ * An example of searching for a book
+ */
+// var x = testBookApi("Harry Potter", "").then(result => {
+//     console.log(result); //prints out the json response
+// }).catch(error => {
+
+// })
