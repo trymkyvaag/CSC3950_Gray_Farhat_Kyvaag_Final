517f9e5e1c352e0634e9f0d89327409721a580a6
farhatdmc <farhatd@merrimack.edu>
2023-12-13 19:08:07 -0500

    Updated routing. Added endpoints for search page and future bookshelves page.

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/pages/home.html'));
 });
 
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/pages/search.html'));
+});
+
+app.get('/bookshelves', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/pages/bookshelves.html'));
+});
+
 app.get('/profile', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/pages/signin.html'));
 });
